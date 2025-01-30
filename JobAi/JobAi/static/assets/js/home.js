899bf6d1d6eef993b83a682ef7588f9c37f201d9
prudhwiraj1 <tecknohow.132@gmail.com>
2025-01-30 12:11:06 +0530

    24th Commit

--- JobAi/JobAi/static/assets/js/home.js
+++ JobAi/JobAi/static/assets/js/home.js
@@ -1,6 +1,15 @@
-function select(element)
-{
-    document.querySelectorAll('.option').forEach(opt=>opt.classList.remove('select'));
-    element.classList.add('select');
+document.addEventListener("DOMContentLoaded", function () {
+  let savedTab = localStorage.getItem("activeTab");
+  if (savedTab) {
+      document.querySelector(`.box[href='${savedTab}']`)?.classList.add("active");
+  }
 
-}
+  document.querySelectorAll(".box").forEach(link => {
+    link.addEventListener("click", function () {
+      document.querySelectorAll(".box").forEach(el => el.classList.remove("active"));
+      this.classList.add("active");
+      localStorage.setItem("activeTab", this.getAttribute("href"));
+    });
+  });
+});
+
