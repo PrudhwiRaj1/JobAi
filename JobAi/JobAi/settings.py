07cf582ae33c42142fb556746a414891e550792a
Prudhwi Raj Krishna V <174160463+PrudhwiRaj1@users.noreply.github.com>
2025-02-22 06:36:21 +0530

    Update settings.py

--- JobAi/JobAi/settings.py
+++ JobAi/JobAi/settings.py
@@ -143,4 +143,4 @@ EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 EMAIL_HOST_USER = 'tecknohow.132@gmail.com'
-EMAIL_HOST_PASSWORD = 'Qsdr345#@123'
+EMAIL_HOST_PASSWORD = 'password'
